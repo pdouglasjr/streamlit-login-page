6af9c3a2ef8aeb35f88238b51c42c91902c03bdc
Phillip Douglas <pdouglasjr@proton.me>
2024-11-10 01:25:22 -0500

    updates

--- app.py
+++ app.py
@@ -1,8 +1,13 @@
 import streamlit as st
+from streamlit_authenticator.utilities.hasher import Hasher
 
-# create username/password input boxes
-st.text_input("Email Address:")
-st.text_input("Password:")
+# logon form
+with st.form(key="logon_form"):
+    email = st.text_input("Email Address:")
+    password = st.text_input("Password:")
+    
+    hashed_password = Hasher.hash(password)
 
-# create button
-st.button("Log In")
+    if st.form_submit_button("Login"):
+        st.text(f'Email: {email}')
+        st.text(f'Password: {hashed_password}')
